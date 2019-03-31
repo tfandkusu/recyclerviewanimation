1fda3328a05f1708988ec398dbd52e77aae9c99b
toya <mail@ichiwear.jp>
2019-04-01 06:24:06 +0900

    アニメーション付きで表示配列変更ができるようになった。

--- app/src/main/java/com/example/recyclerviewanimation/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewanimation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.recyclerviewanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +10,8 @@ import com.example.recyclerviewanimation.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler = Handler()
 
     private val viewModel = MainViewModel()
 
@@ -19,6 +22,41 @@ class MainActivity : AppCompatActivity() {
 
         list.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         list.setHasFixedSize(true)
-        list.adapter = MainListAdapter()
+        val adapter = MainListAdapter()
+        list.adapter = adapter
+
+        // 初期データ投入
+        val list = mutableListOf<Store>()
+        for (i in 1..10) {
+            val place = "赤坂" + i + "号店"
+            list.add(Store("RNIマート", place))
+        }
+        adapter.items.addAll(list)
+        // 追加データ投入
+        handler.postDelayed({
+            val list = mutableListOf<Store>()
+            for (i in 1..3) {
+                val place = "追加$i"
+                list.add(Store("RNIマート", place))
+            }
+            adapter.items.addAll(5, list)
+        }, 2000)
+        // 削除する
+        handler.postDelayed({
+            adapter.items.removeAt(9)
+            adapter.items.removeAt(9)
+            adapter.items.removeAt(9)
+        }, 4000)
+        // 変更する
+        handler.postDelayed({
+            val list = mutableListOf<Store>()
+            for (i in 1..3) {
+                val place = "変更$i"
+                list.add(Store("RNIマート", place))
+            }
+            for(i in 0..2)
+                adapter.items[1 + i] = list[i]
+
+        }, 6000)
     }
 }
--- app/src/main/java/com/example/recyclerviewanimation/MainListAdapter.kt
+++ app/src/main/java/com/example/recyclerviewanimation/MainListAdapter.kt
@@ -2,17 +2,62 @@ package com.example.recyclerviewanimation
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableList
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * RecyclerViewのアダプター。itemsの更新でnotify*メソッドによるアニメーションを行える。
+ */
 class MainListAdapter : RecyclerView.Adapter<MainListViewHolder>() {
 
-    private val list = mutableListOf<Store>()
+    /**
+     * 表示するデータ
+     */
+    val items = ObservableArrayList<Store>()
 
     init {
-        for(i in 1..10){
-            val place = "赤坂" + i + "号店"
-            list.add(Store("RNIマート",place))
-        }
+        // 更新監視
+        items.addOnListChangedCallback(object : ObservableList.OnListChangedCallback<ObservableList<Store>>(){
+
+            /**
+             * 初回挿入フラグ
+             */
+            private var firstInsert = true
+
+            override fun onChanged(sender: ObservableList<Store>?) {
+                // 実装しない
+            }
+            override fun onItemRangeMoved(
+                sender: ObservableList<Store>?,
+                fromPosition: Int,
+                toPosition: Int,
+                itemCount: Int
+            ) {
+                // 実装しない
+
+            }
+
+            override fun onItemRangeInserted(sender: ObservableList<Store>?, positionStart: Int, itemCount: Int) {
+                // 初回挿入のみアニメーションを無効化する
+                if(firstInsert) {
+                    notifyDataSetChanged()
+                    firstInsert = false
+                }
+                else {
+                    notifyItemRangeInserted(positionStart, itemCount)
+                }
+            }
+
+            override fun onItemRangeChanged(sender: ObservableList<Store>?, positionStart: Int, itemCount: Int) {
+                notifyItemRangeChanged(positionStart,itemCount)
+            }
+
+
+            override fun onItemRangeRemoved(sender: ObservableList<Store>?, positionStart: Int, itemCount: Int) {
+                notifyItemRangeRemoved(positionStart,itemCount)
+            }
+        })
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainListViewHolder {
@@ -21,11 +66,11 @@ class MainListAdapter : RecyclerView.Adapter<MainListViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return items.size
     }
 
     override fun onBindViewHolder(vh: MainListViewHolder, position: Int) {
-        vh.viewModel.chain.set(list[position].chain)
-        vh.viewModel.place.set(list[position].place)
+        vh.viewModel.chain.set(items[position].chain)
+        vh.viewModel.place.set(items[position].place)
     }
 }
